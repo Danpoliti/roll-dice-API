1b6a8d5b4c01a34b23a6f850d6218c0c2e8945f8
Danpoliti <danielcpoliti@yahoo.com.br>
2021-05-30 20:35:18 -0400

    message css style

--- js/script.js
+++ js/script.js
@@ -20,23 +20,23 @@ document.addEventListener("DOMContentLoaded", function () {
           console.log(response.data);
           diceNumber.innerHTML = response.data.Dice;
           if (response.data.Dice === 2) {
-            var imageshown = "img/inverted-dice-2.svg"
-            document.getElementById("img").src = imageshown;
+            var img = "img/inverted-dice-2.svg"
+            document.getElementById("img").src = img;
             } else if (response.data.Dice === 1){
-              var imageshown = "img/inverted-dice-1.svg"
-              document.getElementById("img").src = imageshown;
+              var img = "img/inverted-dice-1.svg"
+              document.getElementById("img").src = img;
             } else if (response.data.Dice === 3){
-              var imageshown = "img/inverted-dice-3.svg"
-              document.getElementById("img").src = imageshown;
+              var img = "img/inverted-dice-3.svg"
+              document.getElementById("img").src = img;
             } else if (response.data.Dice === 4){
-              var imageshown = "img/inverted-dice-4.svg"
-              document.getElementById("img").src = imageshown;
+              var img = "img/inverted-dice-4.svg"
+              document.getElementById("img").src = img;
             } else if (response.data.Dice === 5){
-              var imageshown = "img/inverted-dice-5.svg"
-              document.getElementById("img").src = imageshown;
+              var img = "img/inverted-dice-5.svg"
+              document.getElementById("img").src = img;
             } else if (response.data.Dice === 6){
-              var imageshown = "img/inverted-dice-6.svg"
-              document.getElementById("img").src = imageshown;
+              var img = "img/inverted-dice-6.svg"
+              document.getElementById("img").src = img;
             }
           rollMessage.innerHTML = response.data.talk;
         })
